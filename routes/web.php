dae92c9d16640c8abe9cee845de568d934aaccc8
fikrifrramdani <fikriramdani32@gmail.com>
2022-06-25 16:24:32 +0700

    Mengubah editorconfig

--- routes/web.php
+++ routes/web.php
@@ -14,77 +14,77 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home', [
-        "title" => "Home"
-    ]);
+	return view('home', [
+		"title" => "Home"
+	]);
 });
 
 Route::get('/about', function () {
-    return view('about', [
-        "title" => "About",
-        "name" => "Fikri Ramdani",
-        "email" => "fikriramdani@mainoffice.com",
-        "image" => "1.png"
-    ]);
+	return view('about', [
+		"title" => "About",
+		"name" => "Fikri Ramdani",
+		"email" => "fikriramdani@mainoffice.com",
+		"image" => "1.png"
+	]);
 });
 
 Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Fikri Ramdani",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, delectus alias nostrum aut odio temporibus, eum, natus
+	$blog_posts = [
+		[
+			"title" => "Judul Post Pertama",
+			"slug" => "judul-post-pertama",
+			"author" => "Fikri Ramdani",
+			"body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, delectus alias nostrum aut odio temporibus, eum, natus
             recusandae quam modi dolore rerum esse numquam in repudiandae perferendis accusamus minima totam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, delectus alias nostrum aut odio temporibus, eum, natus
             recusandae quam modi dolore rerum esse numquam in repudiandae perferendis accusamus minima totam?"
-        ],
+		],
 
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Fikri Ramdani",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, delectus alias nostrum aut odio temporibus, eum, natus
+		[
+			"title" => "Judul Post Kedua",
+			"slug" => "judul-post-kedua",
+			"author" => "Fikri Ramdani",
+			"body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, delectus alias nostrum aut odio temporibus, eum, natus
             recusandae quam modi dolore rerum esse numquam in repudiandae perferendis accusamus minima totam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, delectus alias nostrum aut odio temporibus, eum, natus
             recusandae quam modi dolore rerum esse numquam in repudiandae perferendis accusamus minima totam?"
-        ]
-    ];
-    return view('posts', [
-        "title" => "Blog",
-        "posts" => $blog_posts
-    ]);
+		]
+	];
+	return view('posts', [
+		"title" => "Blog",
+		"posts" => $blog_posts
+	]);
 });
 
 // halaman single posts
 Route::get('posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Fikri Ramdani",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, delectus alias nostrum aut odio temporibus, eum, natus
+	$blog_posts = [
+		[
+			"title" => "Judul Post Pertama",
+			"slug" => "judul-post-pertama",
+			"author" => "Fikri Ramdani",
+			"body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, delectus alias nostrum aut odio temporibus, eum, natus
             recusandae quam modi dolore rerum esse numquam in repudiandae perferendis accusamus minima totam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, delectus alias nostrum aut odio temporibus, eum, natus
             recusandae quam modi dolore rerum esse numquam in repudiandae perferendis accusamus minima totam?"
-        ],
+		],
 
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Fikri Ramdani",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, delectus alias nostrum aut odio temporibus, eum, natus
+		[
+			"title" => "Judul Post Kedua",
+			"slug" => "judul-post-kedua",
+			"author" => "Fikri Ramdani",
+			"body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, delectus alias nostrum aut odio temporibus, eum, natus
             recusandae quam modi dolore rerum esse numquam in repudiandae perferendis accusamus minima totam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, delectus alias nostrum aut odio temporibus, eum, natus
             recusandae quam modi dolore rerum esse numquam in repudiandae perferendis accusamus minima totam?"
-        ]
-    ];
+		]
+	];
 
-    $new_post = [];
-    foreach ($blog_posts as $post) {
-        if ($post["slug"] == $slug) {
-            $new_post = $post;
-        }
-    }
+	$new_post = [];
+	foreach ($blog_posts as $post) {
+		if ($post["slug"] == $slug) {
+			$new_post = $post;
+		}
+	}
 
-    return view('post', [
-        "title" => "Single post",
-        "post" => $new_post
-    ]);
+	return view('post', [
+		"title" => "Single post",
+		"post" => $new_post
+	]);
 });
